26cd55c39e4e808eab8199eead7827a26ef1f034
gabrieli2300 <lela.j1977@gmail.com>
2025-09-01 15:26:24 +0400

    Add files via upload

---
+++ goa cors/day009/class work/cw.py
@@ -0,0 +1,4 @@
+#            True       True     True    False
+#        False or True and True or True and False   
+#    ((1        (0)     (1)       (1)     (1)        0))
+print(True and False or True and True or True and False)
